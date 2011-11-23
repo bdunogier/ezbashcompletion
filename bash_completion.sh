7d75942ed926c40018ed68114d29ffb32116c81e
Bertrand Dunogier <bd@ez.no>
2011-11-23 12:32:55 +0100

    Moved forwad on #3 and #5: completion of args with =

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,3 +1,80 @@
+__ezp_reassemble_comp_words_by_ref()
+{
+	local exclude i j first
+	# Which word separators to exclude?
+	exclude="${1//[^$COMP_WORDBREAKS]}"
+	cword_=$COMP_CWORD
+	if [ -z "$exclude" ]; then
+		words_=("${COMP_WORDS[@]}")
+		return
+	fi
+	# List of word completion separators has shrunk;
+	# re-assemble words to complete.
+	for ((i=0, j=0; i < ${#COMP_WORDS[@]}; i++, j++)); do
+		# Append each nonempty word consisting of just
+		# word separator characters to the current word.
+		first=t
+		while
+			[ $i -gt 0 ] &&
+			[ -n "${COMP_WORDS[$i]}" ] &&
+			# word consists of excluded word separators
+			[ "${COMP_WORDS[$i]//[^$exclude]}" = "${COMP_WORDS[$i]}" ]
+		do
+			# Attach to the previous token,
+			# unless the previous token is the command name.
+			if [ $j -ge 2 ] && [ -n "$first" ]; then
+				((j--))
+			fi
+			first=
+			words_[$j]=${words_[j]}${COMP_WORDS[i]}
+			if [ $i = $COMP_CWORD ]; then
+				cword_=$j
+			fi
+			if (($i < ${#COMP_WORDS[@]} - 1)); then
+				((i++))
+			else
+				# Done.
+				return
+			fi
+		done
+		words_[$j]=${words_[j]}${COMP_WORDS[i]}
+		if [ $i = $COMP_CWORD ]; then
+			cword_=$j
+		fi
+	done
+}
+
+if ! type _get_comp_words_by_ref >/dev/null 2>&1; then
+_get_comp_words_by_ref ()
+{
+	local exclude cur_ words_ cword_
+	if [ "$1" = "-n" ]; then
+		exclude=$2
+		shift 2
+	fi
+	__ezp_reassemble_comp_words_by_ref "$exclude"
+	cur_=${words_[cword_]}
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		cur)
+			cur=$cur_
+			;;
+		prev)
+			prev=${words_[$cword_-1]}
+			;;
+		words)
+			words=("${words_[@]}")
+			;;
+		cword)
+			cword=$cword_
+			;;
+		esac
+		shift
+	done
+}
+fi
+
+
 _ezp()
 {
     local cur prev opts
@@ -8,18 +85,22 @@ _ezp()
     fi
 
     COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
+    #cur=$(_get_cword "=")
+    _get_comp_words_by_ref -n =: cur prev
+
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    if [ "${cur}" = "=" ]; then
-        #todo split on = to get a valid $cur
-        prev="${prev}="
-        cur=""
-    fi
+    _ezp_p_debug "COMP_WORDS: ${COMP_WORDS[*]} | PREV: ${prev} | CUR: ${cur}"
 
-    _ezp_p_debug "PREV: ${prev}"
-
-    case "${prev}" in
+    case "$cur" in
+        --siteaccess=*)
+	    _ezp_exec "_siteaccess_list"
+	    _ezp_complete "${exec_result}" "${cur##--siteaccess=}"
+            return 0
+	;;
+    esac
+   
+    case "$prev" in
 
         # completion for ezp command: script names
         ezp)
@@ -30,16 +111,16 @@ _ezp()
             ;;
 
         # siteaccess completion
-	--siteaccess= | -s)
+	--siteaccess | -s)
 	    _ezp_exec "_siteaccess_list"
-	    _ezp_complete "${exec_result}" "${cur}"
+	    _ezp_complete "${exec_result}" "${cur##--siteaccess=}"
             return 0
 	    ;;
 
         # ezcache.php --clear-tag=
         --clear-tag=)
             _ezp_exec "_ezcache_tags"
-            _ezp_complete "${exec_result}" "${cur}"     
+            _ezp_complete "${exec_result}" "${cur}"
             return 0
             ;;
 
@@ -68,8 +149,9 @@ complete -o default -o nospace -F _ezp ezp 2>>/dev/null || -o default -o nospace
 # @param $2 Current word
 _ezp_complete()
 {
+    _ezp_p_debug "_ezp_complete '$1' '$2'"
     local IFS=$'\n'
-    COMPREPLY=( $(compgen -W "$1" -- $2 ) )
+    COMPREPLY=( $(compgen -W "$1" -- "$2" ) )
 }
 
 # Executes ezp.php with the given arguments
